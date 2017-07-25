d15047d6de21ff040c9d4ed0275441e73dcd1f72
Jason Seibel <jasonLseibel@gmail.com>
2017-07-25 12:50:49 -0700

    all eslint and webpack setup integrated with react now

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,29 @@
 */
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './src/index.html',
   filename: 'index.html',
-  inject: 'body'
+  inject: 'body',
 })
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   module: {
     loaders: [
       { test: /\.js$/, loaders: ['babel-loader', 'eslint-loader'], exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
       {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader'
-      }
-    ]
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+    ],
   },
-  plugins: [HtmlWebpackPluginConfig]
+  plugins: [HtmlWebpackPluginConfig],
 }
